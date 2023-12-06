49a8e06817e919142865193061aba4bef5f4d0a6
Emre Akyüz <aemreakyuz@gmail.com>
2023-12-06 22:42:18 +0300

    productPage structured

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Header from "../layout/Header";
+import NavBar from "../layout/NavBar";
+import Icons from "../components/Icons";
+import Footer from "../layout/Footer";
 
 function Product() {
   return (
-    <div>Product</div>
-  )
+    <div>
+      <Header />
+      <NavBar />
+      <Icons />
+      <Footer />
+    </div>
+  );
 }
 
-export default Product
+export default Product;
