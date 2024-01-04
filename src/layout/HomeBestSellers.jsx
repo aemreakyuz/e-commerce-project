72c11118ba38e6ddbe319e10314869fa527d831b
Emre Akyüz <aemreakyuz@gmail.com>
2024-01-04 19:40:08 +0300

    test

--- src/layout/HomeBestSellers.jsx
+++ src/layout/HomeBestSellers.jsx
@@ -13,7 +13,7 @@ function HomeBestSellers() {
           BESTSELLER PRODUCTS
         </h2>
         <h4 className="text-[14px] text-[#737373] text-center">
-          Problems trying to resolve the conflict between vjıreobjvfrelre
+          Problems trying to resolve the conflict between testtest
         </h4>
       </div>
       <div className="">
